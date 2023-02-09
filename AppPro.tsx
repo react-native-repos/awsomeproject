a3ea2743d0a68368b96f22c68c5c99564504ed23
sudeepsawant <sudeepsawant3710@gmail.com>
2023-02-09 14:29:42 +0530

    added styling on views

---
+++ AppPro.tsx
@@ -0,0 +1,41 @@
+import React  from "react";
+import {
+    View,
+    Text,
+    StyleSheet, // component for styling
+    useColorScheme  // For Dark and Light mode customization
+} from 'react-native'
+
+// This will always return jsx element
+function AppPro():JSX.Element{
+    const isDarkMode = useColorScheme() === 'light'
+
+    // Applying styles on views
+    return(
+        <View style={styles.container}>
+            <Text style={styles.darkRedText}>Hello World ! </Text>
+        </View>
+    )
+}
+
+// creating styles (Takes an object of key value pairs)
+const styles = StyleSheet.create({
+    // binding key & value pairs
+    container:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent:'center'
+    },
+    lightText:{
+        color: '#FFFFFF',
+    },
+    darkText: {
+        color: '#000000',
+    },
+    darkRedText:{
+        color: '#8B0000'
+    }
+})
+
+export default AppPro
+
